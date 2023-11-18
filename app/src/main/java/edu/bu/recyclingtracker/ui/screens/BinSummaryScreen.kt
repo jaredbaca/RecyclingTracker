31e80c48530d8a1ad3119e4106fc2ebb0bfc7f81
Jared Baca <110132943+jaredjames90@users.noreply.github.com>
2023-11-18 16:09:20 -0500

    added fab

--- app/src/main/java/edu/bu/recyclingtracker/ui/screens/BinSummaryScreen.kt
+++ app/src/main/java/edu/bu/recyclingtracker/ui/screens/BinSummaryScreen.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Upload
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -36,6 +40,12 @@ fun BinSummaryScreen(navController: NavController, viewModel: LogRecyclablesView
         },
         bottomBar = {
             bottomNavBar2(navItems = navItems, navController)
+        } ,
+                
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Upload, contentDescription = null)
+            }
         }
 
     ) {paddingValues ->

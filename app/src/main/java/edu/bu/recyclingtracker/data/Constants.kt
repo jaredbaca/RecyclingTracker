397e487556dc5f204a84e973a8ecf7dba3efa52c
Jared Baca <110132943+jaredjames90@users.noreply.github.com>
2023-11-23 10:13:24 -0500

    added new recyclable items

--- app/src/main/java/edu/bu/recyclingtracker/data/Constants.kt
+++ app/src/main/java/edu/bu/recyclingtracker/data/Constants.kt
@@ -3,18 +3,30 @@ package edu.bu.recyclingtracker.data
 import androidx.compose.runtime.mutableStateOf
 import edu.bu.recyclingtracker.R
 
-//val recyclables = listOf(
-//    "Aluminum Can",
-//    "Glass Bottle",
-//    "Plastic Bottle"
-//)
-
 val recyclables = mutableStateOf( listOf(
-    RecyclingItemUiState("Plastic Bottle", "Plastic" ,icon = R.drawable.plastic_bottle),
+    //Plastics
+    RecyclingItemUiState("12oz Bottle", "Plastic" ,icon = R.drawable.plastic_bottle),
+    RecyclingItemUiState("2-Liter Bottle", "Plastic"),
+    RecyclingItemUiState("Takeout Box", "Plastic"),
+    RecyclingItemUiState("Soap Bottles", "Plastic"),
+    RecyclingItemUiState("Milk Jug", "Plastic"),
+
+    //Metals
     RecyclingItemUiState("Aluminum Can", "Metal", icon = R.drawable.aluminum_can),
-    RecyclingItemUiState("Soup Can", "Metal", icon = R.drawable.aluminum_can),
-    RecyclingItemUiState("Glass Bottle", "Glass", icon = R.drawable.glass_bottle),
-    RecyclingItemUiState("Small Cardboard Box", "Cardboard")
+    RecyclingItemUiState("Soup Can", "Metal"),
+
+    //Glass
+    RecyclingItemUiState("Beer Bottle", "Glass", icon = R.drawable.glass_bottle),
+    RecyclingItemUiState("Wine Bottle", "Glass"),
+    RecyclingItemUiState("Mason Jar", "Glass"),
+    RecyclingItemUiState("Pasta Sauce Jar", "Glass"),
+
+    //Cardboard
+    RecyclingItemUiState("Small Cardboard Box", "Cardboard"),
+    RecyclingItemUiState("Medium Cardboard Box", "Cardboard"),
+    RecyclingItemUiState("Large Cardboard Box", "Cardboard"),
+    RecyclingItemUiState("Pizza Box", "Cardboard"),
+    RecyclingItemUiState("Frozen Food Box", "Cardboard"),
 ))
 
 //val metals = mutableStateOf( listOf(

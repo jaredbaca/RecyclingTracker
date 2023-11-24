ec7fbc615ca946c0baf99d0482e7fea86242f434
Jared Baca <110132943+jaredjames90@users.noreply.github.com>
2023-11-24 18:13:31 -0500

    added pie chart template

--- app/src/main/java/edu/bu/recyclingtracker/ui/screens/StatsScreen.kt
+++ app/src/main/java/edu/bu/recyclingtracker/ui/screens/StatsScreen.kt
@@ -4,17 +4,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import edu.bu.recyclingtracker.data.navItems
 import edu.bu.recyclingtracker.ui.LogRecyclablesViewModel
 import edu.bu.recyclingtracker.ui.components.AppToolbar
+import edu.bu.recyclingtracker.ui.components.PieChart
 import edu.bu.recyclingtracker.ui.components.bottomNavBar2
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -40,21 +44,50 @@ fun StatsScreen(navController: NavController, viewModel: LogRecyclablesViewModel
                 .fillMaxSize()
                 .padding(paddingValues)
         ) {
-            Column {
-                Row {
-                    Text(
-                        text = "Stats Screen",
-                        fontSize = 24.sp
+//            Column {
+//                Row {
+//                    Text(
+//                        text = "Stats Screen",
+//                        fontSize = 24.sp
+//                    )
+//                }
+//
+//                viewModel.totals.value.forEach {
+//                    Row {
+//                        Text(text = "${it.key}: ${it.value}")
+//                    }
+//                }
+//
+//                Row {
+//                    PieChart(
+//                        data = mapOf(
+//                            Pair("Sample-1", 150),
+//                            Pair("Sample-2", 120),
+//                            Pair("Sample-3", 110),
+//                            Pair("Sample-4", 170),
+//                            Pair("Sample-5", 120),
+//                        )
+//                    )
+//                }
+//            }
+
+            LazyColumn {
+                item {
+                    Divider()
+                }
+                item {
+                    PieChart(
+                        data = mapOf(
+                            Pair("Sample-1", 150),
+                            Pair("Sample-2", 120),
+                            Pair("Sample-3", 110),
+                            Pair("Sample-4", 170),
+                            Pair("Sample-5", 120),
+                        )
                     )
                 }
 
-                viewModel.totals.value.forEach {
-                    Row {
-                        Text(text = "${it.key}: ${it.value}")
-                    }
-                }
-            }
-        }
+            } // Lazy Column
+        } // Surface
     }
-
 }

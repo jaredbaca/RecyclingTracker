59c82d4f876ec48c926e3c3ba6f47d0929501f3a
Jared Baca <110132943+jaredjames90@users.noreply.github.com>
2023-12-01 20:55:48 -0500

    added new colors

--- app/src/main/java/edu/bu/recyclingtracker/ui/theme/Color.kt
+++ app/src/main/java/edu/bu/recyclingtracker/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package edu.bu.recyclingtracker.ui.theme
 
+import android.graphics.Color.parseColor
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +10,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Forest_Green = Color(parseColor("#08392B"))
+val Teal_Green = Color(parseColor("#578E87"))
+val Cream = Color(parseColor("#F7E2C7"))
+
+//val Green = Color(0x29660C)
+
+val Green = Color(parseColor("#29660C"))

4854a72a3b3a4a5770bc0e0e826b6992a76bc4c1
Jared Baca <110132943+jaredjames90@users.noreply.github.com>
2023-12-06 13:29:36 -0500

    started adding login before switching to login branch

--- app/src/main/java/edu/bu/recyclingtracker/ui/screens/LoginScreen.kt
+++ app/src/main/java/edu/bu/recyclingtracker/ui/screens/LoginScreen.kt
@@ -1,9 +1,35 @@
 package edu.bu.recyclingtracker.ui.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen() {
-    Text("Login Screen")
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding()
+    ) {
+
+        var username by remember { mutableStateOf("Username") }
+
+        TextField(value = username, onValueChange = {
+                text -> username = text
+        })
+
+    }
+
+
 }

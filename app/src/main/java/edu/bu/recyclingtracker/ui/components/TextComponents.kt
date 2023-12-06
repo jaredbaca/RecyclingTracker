b311a12d241e701707fc9a3e636cd907750fbd47
Jared Baca <110132943+jaredjames90@users.noreply.github.com>
2023-12-06 18:03:43 -0500

    added login fields and button, do not work yet

--- app/src/main/java/edu/bu/recyclingtracker/ui/components/TextComponents.kt
+++ app/src/main/java/edu/bu/recyclingtracker/ui/components/TextComponents.kt
@@ -3,12 +3,19 @@ package edu.bu.recyclingtracker.ui.components
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -55,4 +62,17 @@ fun drawerItem(name: String,
         }
         scope.launch { drawerState.apply { close() } }
     })
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun loginField(label: String, keyboardType: KeyboardOptions) {
+    var text by remember { mutableStateOf("") }
+    OutlinedTextField(value = text,
+        onValueChange = {
+            text = it
+        },
+        label = {Text(label)},
+        keyboardOptions = keyboardType
+    )
 }
--- app/src/main/java/edu/bu/recyclingtracker/ui/screens/LoginScreen.kt
+++ app/src/main/java/edu/bu/recyclingtracker/ui/screens/LoginScreen.kt
@@ -1,9 +1,52 @@
 package edu.bu.recyclingtracker.ui.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import edu.bu.recyclingtracker.ui.components.loginField
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen() {
-    Text("Login Screen")
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding()
+    ) {
+        Column(verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            loginField(label = "Username", keyboardType = KeyboardOptions.Default)
+            loginField(label = "Password", keyboardType = KeyboardOptions(keyboardType = KeyboardType.Password))
+            Button(modifier = Modifier.padding(16.dp), onClick = { /*TODO*/ }) {
+                Text("Login")
+            }
+            Row {
+                Text(text = "or", modifier = Modifier.padding(bottom = 16.dp))
+            }
+            Text(text = "Create Account", modifier = Modifier.clickable {
+
+            })
+
+        }
+    }
 }
